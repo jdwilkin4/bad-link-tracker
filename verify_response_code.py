ea0c7dbc62cbe1f7f7b59e49365c7659ece5ae30
jdwilkin4 <jwilkin4@hotmail.com>
2023-03-24 11:27:23 -0700

    wip: doesnt matter some changes

--- verify_response_code.py
+++ verify_response_code.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 
 import requests
+import urllib.parse
 
 url = input("Enter your url: ")
 page = requests.get(url)
@@ -11,17 +12,22 @@ num_broken_img_links = 0
 
 for img in soup.find_all('img'):
     original_img_url = img.get('src')
-    img_url = (requests.get(original_img_url)).url
-    img_url_response_code = requests.get(img_url).status_code
-    history = requests.get(original_img_url).history
+
+    if not original_img_url.startswith("http"):
+        original_img_url = urllib.parse.urljoin(page.url, original_img_url)
+        print(original_img_url)
+    img_url = requests.get(original_img_url)
+    img_url_response_code = img_url.status_code
+    img_url_history = img_url.history
 
     if img_url_response_code != 200:
         num_broken_img_links = num_broken_img_links + 1
-        print(f"Url: { original_img_url} " + f"| Status Code: {img_url_response_code}")
+        print(f"Url: {original_img_url} " + f"| Status Code: {img_url_response_code}")
 
     if original_img_url == "https://www.primefaces.org/primeng/assets/showcase/images/primeng-logo-dark.svg":
-        print("this is the history for broken angular img " + f"{history}")
+        print("this is the history for broken angular img " + f"{img_url_history}")
         # TODO: add loop to print urls for history
 
 if num_broken_img_links == 0:
     print("No broken links found")
+
