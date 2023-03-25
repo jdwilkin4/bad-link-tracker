614e379f0ee644ca74a97309a334b9e4e2161f6d
jdwilkin4 <jwilkin4@hotmail.com>
2023-03-24 23:14:52 -0700

    feat: updating messaging for broken links function

--- get_broken_links_list.py
+++ get_broken_links_list.py
@@ -11,6 +11,7 @@ def get_broken_links(site):
     num_broken_img_links = 0
     broken_links_list = []
  
+    site_page_title = soup.find('title').get_text()
 
     for img in soup.find_all('img'):
         img_url = img.get('src')
@@ -24,16 +25,17 @@ def get_broken_links(site):
             num_broken_img_links = num_broken_img_links + 1
             broken_links_list.append(img_url)
         else:
-            if img_url.status_code != 200:
+            if img_url.status_code != 200 and img_url.status_code != 408 and img_url.status_code != 403:
                 num_broken_img_links = num_broken_img_links + 1
                 broken_links_list.append(img_url)
             
     
 
     if num_broken_img_links == 0:
-        print("no broken links")
+        print(f'No broken links for {site_page_title}')
         return
     else:
+        print(site_page_title)
         print(broken_links_list)
         return broken_links_list
 
